7624266d7a51136b1c083c988e6593814e4be40e
botus99 <botus99@gmail.com>
2023-08-28 20:08:27 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
-# Inspired by... https://gitlab.com/thelinuxcast/scripts/-/blob/master/setup.sh?ref_type=heads
+### Inspired by... https://gitlab.com/thelinuxcast/scripts/-/blob/master/setup.sh?ref_type=heads
 
 dotfiles="$HOME/.dotfiles"
-gitstuff="$HOME/.github-stuff"
+gitstuff="$HOME/.git-stuff"
 scripts="$HOME/.my-scripts"
 
 mkdir .dotfiles
-mkdir .github-stuff
+mkdir .git-stuff
 mkdir .my-scripts
 
-# install nerd-fonts
+### install nerd-fonts
 cd $gitstuff
 echo -e "\033[36m Installing Nerd Fonts \033[0m"
 echo -e "\033[36m this might take a while... \033[0m"
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
 ./install.sh FiraMono FiraCode Iosevka IosevkaTerm Monofur Mononoki 
 
-# bare nessessities
+### bare nessessities
 echo -e "\033[36m Installing Dependencies \033[0m"
-sudo apt install nala fonts-font-awesome aria2c bash-completion exa lolcat nano -y
+### start with some basics...
+sudo apt install nala fonts-font-awesome aria2 bash-completion exa lolcat nano -y
+### start using nala
 nala --install-completion bash
 sudo nala install sway swayidle swaybg swaylock wayland-utils wayland-protocols xwayland waybar wofi pcmanfm alacritty kitty btop cmatrix ffmpeg optipng mediainfo mediainfo-gui mpd ncmpcpp mpv gtk2-engines-murrine gtk2-engines-pixbuf neofetch pulsemixer python3 pipx tldr git flatpak xdg-user-dirs xdg-utils yt-dlp zip unzip p7zip-full build-essential libpam0g-dev libxcb-xkb-dev
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install com.github.tchx84.Flatseal com.vscodium.codium io.gitlab.librewolf-community org.mozilla.Thunderbird
 
-# dotfile installation
-echo -e "\033[36m Downloading Dotfiles \033[0m"
-cd && git clone https://github.com/botus99/.dotfiles.git
-echo -e "\033[36m Installing Dotfiles \033[0m"
+### install python apps
+pipx install streamrip
 
-cd $dotfiles
+### download & install wallpapers
+echo -e "\033[36m Downloading Wallpapers \033[0m"
+cd ~
+git clone https://github.com/botus99/.wallpapers.git
+
+### dotfile installation
+echo -e "\033[36m Downloading Dotfiles \033[0m"
+cd ~
+git clone https://github.com/botus99/.dotfiles.git
+echo -e "\033[36m Installing Dotfiles \033[0m"
+#cd $dotfiles
 ln -s $HOME/.dotfiles/.config/sway ~/.config
 ln -s $HOME/.dotfiles/.config/swaylock ~/.config
 ln -s $HOME/.dotfiles/.config/alacritty ~/.config
@@ -43,6 +53,5 @@ ln -s $HOME/.dotfiles/.config/btop ~/.config
 #cd $scripts
 #sudo cp *.sh weather.py /usr/local/bin
 
-#cd $HOME/.config
-#git clone https://gitlab.com/thelinuxcast/nvim.git
+### success message
 echo -e "Alllllllllrighty then!!! Looks like we're good here. Just restart me now and we can start the party! Woooooo!!!!"
