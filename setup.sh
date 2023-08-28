53972a9422b254fb6a80ebced7780d299f3f68a3
botus99 <botus99@gmail.com>
2023-08-28 22:23:43 +0000

    Update setup.sh
    more fonts

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
 ### bare nessessities
 echo -e "\033[36m Installing Dependencies \033[0m"
 ### start with some basics...
-sudo apt install nala fonts-font-awesome aria2 bash-completion exa lolcat nano -y
+sudo apt install nala fonts-font-awesome fonts-mononoki fonts-roboto fonts-recommended aria2 bash-completion exa lolcat nano -y
 ### start using nala
 nala --install-completion bash
 sudo nala install sway swayidle swaybg swaylock wayland-utils wayland-protocols xwayland waybar wofi pcmanfm alacritty kitty btop cmatrix ffmpeg optipng mediainfo mediainfo-gui mpd ncmpcpp mpv gtk2-engines-murrine gtk2-engines-pixbuf neofetch pulsemixer python3 pipx tldr git flatpak xdg-user-dirs xdg-utils yt-dlp zip unzip p7zip-full build-essential libpam0g-dev libxcb-xkb-dev
