fd4554592b78e340d78bdc9e5333feafb2d1f467
botus99 <botus99@gmail.com>
2024-06-08 11:43:52 -0500

    add install script for fastfetch
    this will be obsolete eventually as fastfetch will soon be on the debian repos

---
+++ install_fastfetch-linux-amd64.deb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define the base URL for the FastFetch releases
+BASE_URL="https://github.com/fastfetch-cli/fastfetch/releases"
+
+# Find the latest release
+LATEST_RELEASE=$(curl --silent "https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest" | grep '"tag_name":' | sed 's/.*:\s*"\([^"]*\)".*/\1/')
+
+# Construct the download URL
+DOWNLOAD_URL="${BASE_URL}/download/${LATEST_RELEASE}/fastfetch-linux-amd64.deb"
+
+# Download the.deb package
+echo "Downloading the latest FastFetch.deb package..."
+wget "${DOWNLOAD_URL}"
+
+# Prompt the user before installing
+read -p "Do you want to install FastFetch now? [y/N] " answer
+if [[ $answer =~ ^[Yy]$ ]]
+then
+    # Install the package using apt-get to handle dependencies and prompt the user
+    echo "Installing FastFetch..."
+    sudo dpkg -i ./fastfetch-linux-amd64.deb
+else
+    echo "Installation cancelled."
+fi
+
+# Clean up the downloaded.deb file
+rm ./fastfetch-linux-amd64.deb
