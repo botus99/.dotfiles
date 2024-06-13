af32dad41f8f919b8db0d8869c04c73ed6119cca
botus99 <botus99@gmail.com>
2024-06-12 23:41:53 -0500

    update launcher.sh paths

--- .config/rofi/launcher.sh
+++ .config/rofi/launcher.sh
@@ -4,14 +4,10 @@
 ## Github : @adi1090x
 #
 ## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
-#
-## Available Styles
-#
-## style-1     style-2     style-3     style-4     style-5
-## style-6     style-7     style-8     style-9     style-10
 
-dir="$HOME/.config/rofi/launchers/type-7"
-theme='style-1'
+
+dir="$HOME/.config/rofi"
+theme='launcher'
 
 ## Run
 rofi \
