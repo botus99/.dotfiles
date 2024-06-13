4fd0a0b15f2807de58983fc15eea839d4885a217
botus99 <botus99@gmail.com>
2024-06-12 23:43:18 -0500

    create basic-launcher.sh

---
+++ .config/rofi/basic-launcher.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+#
+## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
+
+
+dir="$HOME/.config/rofi"
+theme='basic-launcher'
+
+## Run
+rofi \
+    -show drun \
+    -theme ${dir}/${theme}.rasi
