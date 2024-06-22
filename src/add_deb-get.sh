d50b6855bb7de963f4f5412de9661221925c016f
botus99 <botus99@gmail.com>
2024-06-22 11:10:25 -0500

    create deb-get install script

---
+++ src/add_deb-get.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#=============================================================================#
+#                      GitHub Personal Access Token (PAT)                     #
+#     You must provide deb-get with a GitHub Personal Access Token (PAT).     #
+#    Once created, insert it into the DEBGET_TOKEN environment variable for   #
+#          for deb-get to use for authorization with the GitHub API.          #
+#                                                                             #
+# For example:                                                                #
+#                                                                             #
+#  export DEBGET_TOKEN=github-personal-access-token                           #
+#  deb-get update                                                             #
+#  deb-get upgrade                                                            #
+#                                                                             #
+#                   Skipping this step will lead to failures                  #
+#              during the update, upgrade, and install commands.              #
+#=============================================================================#
+
+sudo apt-get install curl lsb-release wget glab
+curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
