b14591012d34b2346e1e0d5c2c1cfb7d4c8bf8b4
botus99 <botus99@gmail.com>
2024-07-17 17:24:49 +0000

    add script to install wppfzf

---
+++ src/install_wppfzf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install dependencies
+sudo apt-get install fzf ueberzug -y
+# download and install wppfzf
+wget "https://github.com/channel-42/wppfzf/archive/v0.1.0.tar.gz" -O $HOME/wppfzf.tar.gz
+mkdir $HOME/.wppfzf_d
+tar -zxvf wppfzf.tar.gz -C $HOME/.wppfzf_d --strip-components 1
+rm -f $HOME/wppfzf.tar.gz
+cd $HOME/.wppfzf_d
+sudo make install
+# cleanup
+cd $HOME
+rm -rf $HOME/.wppfzf_d
