e95a1c8387fddf98fb55ba4fc963872512509b71
botus99 <botus99@gmail.com>
2024-07-19 20:15:14 -0500

    add shebangs

--- src/install_nf-monofur.sh
+++ src/install_nf-monofur.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mkdir -p "$HOME/.local/share/fonts/NerdFonts" \
 && wget -P "$HOME/.local/share/fonts/NerdFonts" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Monofur.tar.xz" \
 && cd "$HOME/.local/share/fonts/NerdFonts" \
