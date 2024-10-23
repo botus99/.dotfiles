cbd714167fb25f01dec27a56622718bc074aade7
botus99 <botus99@gmail.com>
2024-10-23 16:31:43 -0500

    fix sudo priviledges

--- scripts/add_multimedia-repo.sh
+++ scripts/add_multimedia-repo.sh
@@ -2,7 +2,7 @@
 
 sudo echo "deb https://www.deb-multimedia.org testing main non-free" > deb-multimedia.list
 sudo mv ./deb-multimedia.list /etc/apt/sources.list.d/deb-multimedia.list
-apt-get update -oAcquire::AllowInsecureRepositories=true
-apt-get install deb-multimedia-keyring -y
-apt-get update
-apt-get dist-upgrade
+sudo apt-get update -oAcquire::AllowInsecureRepositories=true
+sudo apt-get install deb-multimedia-keyring -y --allow-unauthenticated
+sudo apt-get update
+sudo apt-get dist-upgrade
