be0fffdab74e38d145314e9bb491fa5cf48310db
botus99 <botus99@gmail.com>
2025-04-11 18:59:22 -0500

    update to .sources format

--- scripts/add_multimedia-repo.sh
+++ scripts/add_multimedia-repo.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-sudo echo "deb https://www.deb-multimedia.org testing main non-free" > deb-multimedia.list
-sudo mv ./deb-multimedia.list /etc/apt/sources.list.d/deb-multimedia.list
+# old way of doing it
+#sudo echo "deb https://www.deb-multimedia.org testing main non-free" > deb-multimedia.list
+#sudo mv ./deb-multimedia.list /etc/apt/sources.list.d/deb-multimedia.list
+
+# create .sources file
+echo '
+Types: deb
+URIs: https://www.deb-multimedia.org
+Suites: unstable
+Components: main non-free
+Signed-By: /usr/share/keyrings/deb-multimedia-keyring.pgp
+Enabled: yes
+' > /etc/apt/sources.list.d/deb-multimedia.sources
+
 sudo apt-get update -oAcquire::AllowInsecureRepositories=true
 sudo apt-get install deb-multimedia-keyring -y --allow-unauthenticated
-sudo apt-get update
-sudo apt-get dist-upgrade
+
--- scripts/add_nvidia-repo.sh
+++ scripts/add_nvidia-repo.sh
@@ -2,4 +2,14 @@
 
 sudo apt-get install dirmngr ca-certificates software-properties-common apt-transport-https dkms curl -y
 curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
-echo 'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/ /' | sudo tee /etc/apt/sources.list.d/nvidia-drivers.list
+
+# old way
+#echo 'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/ /' | sudo tee /etc/apt/sources.list.d/nvidia-drivers.list
+
+echo '
+Types: deb
+URIs: https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/
+Suites: /
+Components: 
+Signed-By: /usr/share/keyrings/nvidia-drivers.gpg
+' > /etc/apt/sources.list.d/nvidia-drivers.sources
