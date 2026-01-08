32aea20de2a2b780a28777a3ea2667b6e5b9ab55
botus99 <botus99@gmail.com>
2026-01-08 09:01:14 -0600

    add quotes

--- scripts/add_drd-team-repo.sh
+++ scripts/add_drd-team-repo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo wget -P /etc/apt/sources.list.d https://debian.drdteam.org/drdteam-$(dpkg --print-architecture).sources
+sudo wget -P /etc/apt/sources.list.d "https://debian.drdteam.org/drdteam-$(dpkg --print-architecture).sources"
 sudo apt-get update
