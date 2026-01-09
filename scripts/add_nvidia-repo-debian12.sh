32d2f444568843937782853806f510cfeeedac6d
botus99 <botus99@gmail.com>
2026-01-09 14:25:35 -0600

    add header

--- scripts/add_nvidia-repo-debian12.sh
+++ scripts/add_nvidia-repo-debian12.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#
+# NVIDIA begins support for Debian 13 - Trixie with the 590 update
+# The 590 update breaks support for some older cards
+# This repo contains the latest driver for cards that can't run the 590 update
 
 sudo apt-get install dirmngr ca-certificates software-properties-common apt-transport-https dkms curl -y
 curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
--- scripts/add_nvidia-repo-debian13.sh
+++ scripts/add_nvidia-repo-debian13.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#
+# NVIDIA begins support for Debian 13 - Trixie with the 590 update
+# The 590 update breaks support for some older cards
+# Please be sure that your card is new enough to support the 590 update before using this repo
 
 sudo apt-get install dirmngr ca-certificates software-properties-common apt-transport-https dkms curl -y
 curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/8793F200.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
